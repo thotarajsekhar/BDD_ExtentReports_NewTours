8656a96f7d847d12988815bcaa3293522a2185e4
Raj Sekhar Thota <sekhar.thota@gmail.com>
2023-10-16 11:45:25 +0530

    Creating a new Test Results Directory Succesful

--- PageObjects/HomePageObjects.cs
+++ PageObjects/HomePageObjects.cs
@@ -1,12 +1,29 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 
 namespace BDD_ExtentReports_NewTours.PageObjects
 {
     public class HomePageObjects
     {
+        internal readonly IWebDriver _driver;
+        private readonly string Page_Title = "Welcome: Mercury Tours";
         public HomePageObjects(IWebDriver driver)
         {
-
+            _driver = driver;
         }
+
+        private IWebElement NewTours_Link => _driver.FindElement(By.LinkText("New Tours"));
+        
+        public void ClickNewToursLink()
+        {
+            // Click on NEw Tours Link Element
+            NewTours_Link.Click();
+        }
+
+        public void VerifyTitleOfPage(IWebDriver driver)
+        {
+            Assert.AreEqual(Page_Title, driver.Title);
+        }
+
     }
 }
---
+++ Utils/ExtentReportUtil.cs
@@ -0,0 +1,19 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using AventStack.ExtentReports.Reporter.Configuration;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace BDD_ExtentReports_NewTours.Utils
+{
+    public class ExtentReportUtils
+    {
+        
+    }
+}
